d22afc2bdb516e0f4d4c2eb47ee4c1b8b55fd936
Julio C. Melchor P <jcmelchorp@gmail.com>
2025-05-04 19:31:48 -0600

    feat: update factory component layout and styles, enhance drag-and-drop functionality, and improve CSS structure

--- src/app/factory/factory.component.ts
+++ src/app/factory/factory.component.ts
@@ -15,27 +15,27 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
 @Component({
   selector: 'app-factory',
   template: `
-  <div class="flex flex-col md:flex-row justify-evenly items-start">
+  <div class="flex flex-col md:flex-row justify-around items-start">
     <div class="w-1/4 mx-5">
-    <div class="flex flex-row justify-evenly items-center">
-    <mat-form-field appearance="outline">
+    <div class="flex flex-col">
+    <div><mat-form-field >
       <mat-label>Columnas</mat-label>
       <input matInput type="number" [(ngModel)]="level.cols"  min="3" max="20" step="1">
-    </mat-form-field>
-    <mat-form-field appearance="outline">
+    </mat-form-field></div>
+    <div><mat-form-field >
       <mat-label>Nivel</mat-label>
       <input matInput type="number" [(ngModel)]="level.level"  min="1"  step="1">
-    </mat-form-field>
-    <mat-form-field appearance="outline">
+    </mat-form-field></div>
+    <div><mat-form-field >
       <mat-label>ID</mat-label>
-      <input matInput  [(ngModel)]="level.id">
-    </mat-form-field>
+      <input matInput type="text"  [(ngModel)]="level.id">
+    </mat-form-field></div>
    </div>
    <h2>Formas</h2>
    <mat-grid-list cols="4" cdkDropList #todoList="cdkDropList" [cdkDropListData]="todo"  rowHeight="100px"
-          [cdkDropListConnectedTo]="doneList" class="mat-grid-list"  
+          [cdkDropListConnectedTo]="doneList" class="mat-grid-list" [ngStyle]="{'width':4*100+'px'}"  
        >
-          <mat-grid-tile *ngFor="let item of todo" class="mat-grid-tile" cdkDrag
+          <mat-grid-tile *ngFor="let item of todo" class="mat-grid-item" cdkDrag
           (cdkDragExited)="exited($event)"
           (cdkDragEntered)="entered($event)"
           
@@ -48,30 +48,45 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
           }"></mat-grid-tile>
       </mat-grid-list>
       <div>
-        <pre>{{level |json }}</pre>
+
+        <pre *ngFor="let block of level.blocks">{{ {rot:block.rot,type:block.type}| json}}</pre>
+        <mat-grid-list class="mat-grid-list"  cols="{{ level.cols }}">
+        <ng-container *ngFor="let tile of level.blocks; index as i">
+          <mat-grid-tile class="mat-grid-tile" 
+          [ngStyle]="{
+              filter: (themeService.appTheme() == 'dark') ? 'invert(0%)' : 'invert(100%)',
+              background: 'center / cover no-repeat url(' + tile.image + ')',
+            }">
+            <div><span></span></div>
+          </mat-grid-tile>
+        </ng-container>
+      </mat-grid-list>
       </div>
       </div>
     
-    <mat-card class="w-1/2 mx-5" [ngStyle]="{'max-width':level.cols*105+'px'}">    
+    <mat-card class="w-1/2 mx-5" [ngStyle]="{'max-width':level.cols*110+'px'}">    
       <mat-card-content>
       <mat-grid-list id="new-level" cols="{{ level.cols }}" cdkDropList #doneList="cdkDropList" [cdkDropListData]="level.blocks" rowHeight="100px"
           [cdkDropListConnectedTo]="todoList" class=" mat-grid-list" (cdkDropListDropped)="drop($event)" [ngStyle]="{'max-width':level.cols*100+'px','min-height':level.cols*100+'px'}">
-          <mat-grid-tile  *ngFor="let item of level.blocks; let i=index" class="mat-grid-tile" cdkDrag [@rotateState]="item.rot"
+          <ng-container *ngFor="let item of level.blocks; index as i">
+          <mat-grid-tile   class="mat-grid-item" cdkDrag [@rotateState]="item.rot"
           (click)="rotate(item)"
           [cdkDragData]="item"
-    (cdkDragStarted)="onDragStarted($event,i)"
+    (cdkDragStarted)="onDragStarted($event)"
     (cdkDragMoved)="onDragMove($event)"
-    (cdkDragEnded)="onDragEnded($event,i)"
+    (cdkDragEnded)="onDragEnded($event)"
           [ngStyle]="{
               filter: (themeService.appTheme() == 'dark') ? 'invert(0%)' : 'invert(100%)',
               background: 'center / cover no-repeat url(' + item.image + ')',
                border: '4px dashed darkgrey' ,
               'border-radius':'25px'
             }">
-                      <div class="example-custom-placeholder" *cdkDragPlaceholder></div>
-                      <div class="block" *cdkDragPreview></div>
+                      <div  *cdkDragPreview></div>
             <span class="font-normal text-2xl text-gray-500">{{item.index}}</span>
           </mat-grid-tile >
+          </ng-container>
+          <div class="example-custom-placeholder" *cdkDragPlaceholder></div>
+
       </mat-grid-list>
       </mat-card-content>
   
@@ -79,7 +94,7 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
     </div>
   `,
   styleUrl: './factory.component.css',
-  imports: [NgFor,CdkDrag, CdkDropList,CdkDragPlaceholder,NgStyle,MatTreeModule,MatGridListModule,MatCardModule,FormsModule,ReactiveFormsModule,MatInputModule,JsonPipe],
+  imports: [NgFor,CdkDrag, CdkDropList,CdkDragPlaceholder,NgStyle,MatTreeModule,MatGridListModule,MatCardModule,FormsModule,ReactiveFormsModule,MatInputModule,KeyValuePipe,JsonPipe],
   animations: [
     trigger('rotateState', [
       state('0', style({ transform: 'rotate(0deg)' })),
@@ -114,21 +129,26 @@ constructor(){
  
   drop(event: CdkDragDrop<TileExtended[]>) {
     if (event.previousContainer === event.container) {
+      // event.container.data[event.currentIndex]= {...event.container.data[event.currentIndex], index:event.previousIndex}
+      // event.container.data[event.previousIndex]= {...event.container.data[event.previousIndex], index:event.currentIndex+1};
+
       moveItemInArray(
         event.container.data,
         event.previousIndex,
         event.currentIndex
       );
+      // event.container.data[event.previousIndex]= {...event.container.data[event.previousIndex], index:event.currentIndex};
+
     } else if (event.container.id === 'new-level') {
-        event.previousContainer.data[event.previousIndex]= {...event.previousContainer.data[event.previousIndex], index:this.level.blocks.length+1};
+        // event.previousContainer.data[event.previousIndex]= {...event.previousContainer.data[event.previousIndex], index:event.currentIndex+1};
         copyArrayItem(event.previousContainer.data,
           event.container.data,
           event.previousIndex,
           event.currentIndex);
         }
-      //  else {
-      //   this.level.blocks.splice(event.container.data.indexOf(event.container.data[event.currentIndex]), 1);
-      // }
+       else if (event.container.id !== event.previousContainer.id) {
+        event.container.data.splice(event.currentIndex, 1);
+      }
     
   }
 
@@ -141,7 +161,7 @@ constructor(){
       );
     } else if (event.previousContainer.id !== event.container.id) {
       //this.transferringItem = {...event.previousContainer.data[event.previousIndex], index:this.done.length+1};
-      event.previousContainer.data[event.previousIndex]= {...event.previousContainer.data[event.previousIndex], index:this.level.blocks.length+1};
+      event.previousContainer.data[event.previousIndex]= {...event.previousContainer.data[event.previousIndex], index:event.currentIndex};
       //console.log(this.transferringItem);
    transferArrayItem(
       event.previousContainer.data,
@@ -165,7 +185,7 @@ console.log( this.transferringItem )
     console.log( this.transferringItem )
   }
  
-  onDragStarted(event:CdkDragStart<any>,index:number)
+  onDragStarted(event:CdkDragStart<any>)
   {
     //you can use
     // this.done[index].text="moved"
@@ -174,7 +194,7 @@ console.log( this.transferringItem )
     event.source.data={};
     // event.source.data.text="moving...."
   }
-  onDragEnded(event:CdkDragEnd<any>,index:number)
+  onDragEnded(event:CdkDragEnd<any>)
   {
     event.source.data={}
   }
@@ -185,16 +205,17 @@ console.log( this.transferringItem )
     const yPos = event.pointerPosition.y - this.offset.y;
     el.style.transform = `translate3d(${xPos}px, ${yPos}px, 0)`;
   }
-  rotate( tile: TileExtended){
-      switch (tile.type) {
+  rotate( item: TileExtended){
+  
+      switch (item.type) {
         case TileType.S:
-          tile.rot = (tile.rot! + 90) % 180;
+          item.rot = (item.rot! + 90) % 180;
           break;
           case TileType.B:
-            tile.rot =0;
+            item.rot =0;
             break;
         default:
-          tile.rot = (tile.rot! + 90) % 360;
+          item.rot = (item.rot! + 90) % 360;
           break;
       }
   }
